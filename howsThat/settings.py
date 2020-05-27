f2ac93b2ee6ae3d3166045a8e4e93da4fe1b6037
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-27 12:51:53 +0530

    chore: add cricVis (Django app) to INSTALLED_APPS in settings

--- howsThat/settings.py
+++ howsThat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'cricVis',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
