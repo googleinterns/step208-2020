22cc160f6b2caaac9abca78a26a369f8636a4d51
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-29 14:07:59 +0530

    docs: add comments to URLS

--- cricVis/urls.py
+++ cricVis/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 app_name = 'cricVis'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^fetchGraphData/$', views.fetchGraphData, name='fetchGraphData'),
+    url(r'^$', views.index, name='index'), #URL for home page
+    url(r'^fetchGraphData/$', views.fetchGraphData, name='fetchGraphData'), #URL for GET function to fetch chart response
 ]
