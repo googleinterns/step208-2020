5d1a649825328b9cdad7b0a182c2c8e14e9a0184
IshaGupta18 <ishagupta1828@gmail.com>
2020-05-29 16:48:57 +0530

    fix: fix relative path of firebase-dk.json

--- cricVis/databaseAPI.py
+++ cricVis/databaseAPI.py
@@ -3,7 +3,7 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 
-cred = credentials.Certificate('static/firebase-sdk.json')
+cred = credentials.Certificate('cricVis/static/cricVis/firebase-sdk.json')
 
 firebase_admin.initialize_app(cred, {
 	'databaseURL' : 'https://trial-1-9f3d9.firebaseio.com/'
