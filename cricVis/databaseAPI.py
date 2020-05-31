39ebce3c1a7448fd909f420a38be2df03f3d3c47
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-01 00:05:46 +0530

    feat: add getAllData function to fetch all data for frontend population

--- cricVis/databaseAPI.py
+++ cricVis/databaseAPI.py
@@ -3,10 +3,28 @@ from firebase_admin import credentials
 from firebase_admin import db
 
 
-cred = credentials.Certificate('cricVis/static/cricVis/firebase-sdk.json')
+cred = credentials.Certificate('static/cricVis/firebase-sdk.json')
 
 firebase_admin.initialize_app(cred, {
 	'databaseURL' : 'https://trial-1-9f3d9.firebaseio.com/'
 })
 
 ref = db.reference('/')
+
+x = ref.child("-M8J1KnUyAQY4s7UxoJl").get()
+print(x)
+
+def getAllData():
+	allData = []
+	allMatches = ref.child("MatchDescription").get()
+	for matchID in allMatches:
+		matchData = {}
+		match = allMatches[matchID]
+		matchData["matchID"] = matchID
+		matchData["team1"] = match["team1"]
+		matchData["team2"] = match["team2"]
+		matchData["matchDate"] = match["matchDate"]
+		allData.append(matchData)
+	return allData
+
+
