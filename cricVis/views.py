c9e5566d4f2079c4acc5d17c68892ac5395e5176
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-02 12:51:21 +0530

    refactor: remove dummy data contents

--- cricVis/views.py
+++ cricVis/views.py
@@ -6,7 +6,6 @@ from cricVis.databaseAPI import *
 def index(request):
     # sent a GET request to get match_ID, team1, team2, match date
     # assemble this data like [{match_ID: , team1: , team2: ,date: }, {....}]
-    # allMatches=[{"match_ID":1,"team1":"Sunrisers Hyderbad", "team2": "Mumbai Indians","date":"18/04/2017"},{"match_ID":2,"team1":"Rajasthan Royals", "team2": "Mumbai Indians","date":"20/04/2017"}]
     allMatches = getAllData()
     context = { "allMatches": allMatches}
     return render(request,'cricVis/index.html',context)
@@ -52,10 +51,5 @@ def fetchGraphData(request):
         matchStats = getMatchStats(matchID)
         playersDismissed =  getPlayersDismissed(matchID)
         teams = getTeamNames(matchID)
-
-        # dummy data for testing
-        # matchStats = {"team1":[{"over":1,"runs":5,"cumulativeRuns":15,"runRate":3.4},{"over":2,"runs":5,"cumulativeRuns":25,"runRate":5.4}],"team2":[{"over":2,"runs":5,"cumulativeRuns":17,"runRate":3.6},{"over":4,"runs":15,"cumulativeRuns":35,"runRate":5.8}]}
-        # playersDismissed = {"team1":[{"player_dismissed":"xyz","over":5},{"player_dismissed":"xyzw","over":6}],"team2":[{"player_dismissed":"abcd","over":7},{"player_dismissed":"efg","over":8}]}
-        #teams = {"team1":"abc","team2":"bcd"}
 
         return HttpResponse(json.dumps(getChartResponse(matchID,matchStats,playersDismissed,teams)))
