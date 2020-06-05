4e437a97a696f2d6f82e201bd2d01ba9517f3314
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-06 00:11:51 +0530

    chore: import allauth in settings

--- howsThat/settings.py
+++ howsThat/settings.py
@@ -32,12 +32,17 @@ ALLOWED_HOSTS = ['localhost']
 
 INSTALLED_APPS = [
     'cricVis',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
 ]
 
 MIDDLEWARE = [
