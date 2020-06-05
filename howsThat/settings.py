3be1ec7c0edaff7926ff0f081c2acd9042982960
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-06 00:16:30 +0530

    chore: add SOCIALACCOUNT_PROVIDERS - add Google

--- howsThat/settings.py
+++ howsThat/settings.py
@@ -131,6 +131,18 @@ AUTHENTICATION_BACKENDS = [
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
+SOCIALACCOUNT_PROVIDERS = {
+    'google': {
+        'SCOPE': [
+            'profile',
+            'email',
+        ],
+        'AUTH_PARAMS': {
+            'access_type': 'online',
+        }
+    }
+}
+
 STATIC_URL = '/static/'
 SITE_ID = 1
 LOGIN_REDIRECT_URL = '/cricVis'
