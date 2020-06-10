1d49140adcbb4f642f82ab55beccf98e1ab1006a
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-10 14:55:58 +0530

    feat: add function to create HTML element

--- cricVis/static/cricVis/script.js
+++ cricVis/static/cricVis/script.js
@@ -1,15 +1,21 @@
   function plotCharts(chartsData){
     // TO-DO: Use the charting library to plot the charts
   }
-  function emptyChartDivs(){
+  function createHTMLElement(elementType,className=null){
+    let element = document.createElement(elementType);
+    if (className) element.classList.add(className);
+    return element;
+  }
+  function emptyMatchElements(){
     $('#WormChartContainer').empty();
     $('#RunRateChartContainer').empty();
     $('#ManhattanChartContainer').empty();
+    $('#team1Details').empty();
+    $('#team2Details').empty();
   }
   function enableChartsDiv(){
     $("#toggleChartsBar").css("visibility","visible");
     $("#chartsContainer").css("visibility","visible");
-    emptyChartDivs();
   }
   // on clicking the "View Results" button, send a GET request to fetchGraphData function in views.py and log the chartsData response.
   $('.match-group .match').click(function(){
@@ -29,9 +35,7 @@
       },
       success: function(allData){
         allData = JSON.parse(allData);
-        console.log(allData);
-        enableChartsDiv();
-        plotCharts(allData["chartData"]);
+        displayMatch(allData);
       },
       error: function(error){
         console.log(error);
