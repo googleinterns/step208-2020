5ae8b6cc11d2e2334a197018321be36b3aeade1c
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-14 22:31:07 +0530

    feat: add function to convert disctionary to matrix

--- cricVis/static/cricVis/iplStats.js
+++ cricVis/static/cricVis/iplStats.js
@@ -1,5 +1,14 @@
 google.charts.load("current", {packages: ['corechart', 'bar']});
 
+function convertDataToMatrix(data,headers){
+  let matrixData = []
+  matrixData.push(headers);
+  for (let xAxis in data){
+    matrixData.push[xAxis, data[xAxis]];
+  }
+  return matrixData;
+}
+
 function receiveData(allIPLData){
   console.log(allIPLData);
 }
