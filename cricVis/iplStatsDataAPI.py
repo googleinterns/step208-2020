2e434ac1a1e46494eab1a91ece1b0120d8d6db5a
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-14 13:46:17 +0530

    feat: add function to fetch match wins for all teams

--- cricVis/iplStatsDataAPI.py
+++ cricVis/iplStatsDataAPI.py
@@ -2,3 +2,9 @@ from cricVis.models import *
 
 def getTeamsData():
     teamsData = db.reference('/TeamWise').get()
+
+def getTeamsWins(teamsData):
+    teamsWins = {}
+    for team in teamsData:
+        teamsWins[team] = teamsData["matchWins"]
+    return teamsWins
