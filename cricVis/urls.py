6892d4fa63f979170376296fc0d2ac30079eda49
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-17 13:45:04 +0530

    chore: add URL for time series graphs page

--- cricVis/urls.py
+++ cricVis/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'), #URL for home page
     url(r'^fetchGraphData/$', views.fetchGraphData, name='fetchGraphData'), #URL for GET function to fetch chart response
     url(r'^iplStats/$', views.iplStats, name='iplStats'),
+    url(r'^timeSeries/$', views.timeSeries, name='timeSeries'),
 ]
