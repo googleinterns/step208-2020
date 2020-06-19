499ddabe8b064f8776a1a3577b1a626a5ca6ee34
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-19 09:56:49 +0530

    feat: add function to get the name of the table

--- cricVis/timeSeriesAPI.py
+++ cricVis/timeSeriesAPI.py
@@ -2,3 +2,6 @@ from cricVis.models import *
 
 def getVisualizationResponse(visualizationRequest):
 
+
+def getTableName(metaDataRequest):
+    return metaDataRequest["type"] + metaDataRequest["format"] +metaDataRequest["gender"] 
