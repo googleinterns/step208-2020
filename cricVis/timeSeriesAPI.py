12860d346b876c88d9719f4d073519c7b7a37744
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-19 10:24:59 +0530

    feat: add function to get response meta data - title, xAxisLabel and yAxisLabel

--- cricVis/timeSeriesAPI.py
+++ cricVis/timeSeriesAPI.py
@@ -11,3 +11,11 @@ def getYearsInRange(startDate, endDate, tableName):
 
 def getTopScoresForAYear(tableName, year, field, top=10):
     return ref.child(tableName).child(year).child(field).order_by_value().limit_to_last(top).get()
+
+def getResponseMetaData(playerType, gameFormat, gender, field, top, startDate, endDate):
+    responseMetaData = {}
+    wordPostion = playerType.find("Performance")
+    responseMetaData["title"] = "Top {} {} in {} {} from {} to {}".format(top, playerType[:wordPostion], playerType, gameFormat, startDate, endDate)
+    responseMetaData["xAxisLabel"] = field
+    responseMetaData["yAxisLabel"] = playerType[wordPostion:]
+    return responseMetaData
