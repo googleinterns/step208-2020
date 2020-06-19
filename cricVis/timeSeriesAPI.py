5fd31ea24d75d5afd1df21be83f36253eb237df4
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-19 09:58:52 +0530

    feat: add function to get the years in the given range

--- cricVis/timeSeriesAPI.py
+++ cricVis/timeSeriesAPI.py
@@ -5,3 +5,6 @@ def getVisualizationResponse(visualizationRequest):
 
 def getTableName(metaDataRequest):
     return metaDataRequest["type"] + metaDataRequest["format"] +metaDataRequest["gender"] 
+
+def getYearsInRange(startDate, endDate, tableName):
+    return ref.child(tableName).order_by_key().start_at(startDate).end_at(endDate).get().keys()
