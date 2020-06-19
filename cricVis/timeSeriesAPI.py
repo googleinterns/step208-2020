1403988e8bb8c6c2ad3208e181b7ba984c3515c8
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-19 10:04:35 +0530

    feat: add function to get the top scores for a given year

--- cricVis/timeSeriesAPI.py
+++ cricVis/timeSeriesAPI.py
@@ -8,3 +8,6 @@ def getTableName(metaDataRequest):
 
 def getYearsInRange(startDate, endDate, tableName):
     return ref.child(tableName).order_by_key().start_at(startDate).end_at(endDate).get().keys()
+
+def getTopScoresForAYear(tableName, year, field, top=10):
+    return ref.child(tableName).child(year).child(field).order_by_value().limit_to_last(top).get()
