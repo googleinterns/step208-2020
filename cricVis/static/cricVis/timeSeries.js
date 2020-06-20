0413bdfd0e57e452963637cfc1a79b7068e4f426
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-21 00:48:12 +0530

    feat: enable batsman and bowler checkboxes

--- cricVis/static/cricVis/timeSeries.js
+++ cricVis/static/cricVis/timeSeries.js
@@ -1,3 +1,11 @@
 $('#startDate').on('input', function(){
     document.getElementById('endDate').disabled = false;
+    $('#endDate').on('input', function(){
+        $('input:checkbox[name=Batsman]').each(function(){
+            $(this).prop('disabled', false);
+        });
+        $('input:checkbox[name=Bowler]').each(function(){
+            $(this).prop('disabled', false);
+        });
+    });
 });
