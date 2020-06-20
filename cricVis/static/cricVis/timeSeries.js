937e01860954876c9d60966d48c413e5bbd95192
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-21 00:50:01 +0530

    refactor: move functions out of nesting

--- cricVis/static/cricVis/timeSeries.js
+++ cricVis/static/cricVis/timeSeries.js
@@ -1,11 +1,11 @@
 $('#startDate').on('input', function(){
     $('#endDate').prop('disabled', false);
-    $('#endDate').on('input', function(){
-        $('input:checkbox[name=Batsman]').each(function(){
-            $(this).prop('disabled', false);
-        });
-        $('input:checkbox[name=Bowler]').each(function(){
-            $(this).prop('disabled', false);
-        });
+});
+$('#endDate').on('input', function(){
+    $('input:checkbox[name=Batsman]').each(function(){
+        $(this).prop('disabled', false);
+    });
+    $('input:checkbox[name=Bowler]').each(function(){
+        $(this).prop('disabled', false);
     });
 });
