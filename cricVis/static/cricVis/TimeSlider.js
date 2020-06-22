0d626badf7e57f188f3045855d0f0b945e93f2f3
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-22 15:16:09 +0530

    chore: create a time slider class

---
+++ cricVis/static/cricVis/TimeSlider.js
@@ -0,0 +1,8 @@
+class TimeSlider{
+  constructor(carouselID, chartDivID, sliderIDNumber, chartData){
+    this.carouselID = carouselID;
+    this.chartDivID = chartDivID;
+    this.sliderIDNumber = sliderIDNumber;
+    this.chartData = chartData;
+  }
+}
