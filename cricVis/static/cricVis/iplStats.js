9264f1e19eb9ce7024a3c4c863f365e486fc3030
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-23 21:48:54 +0530

    refactor: remove redundant code

--- cricVis/static/cricVis/iplStats.js
+++ cricVis/static/cricVis/iplStats.js
@@ -25,17 +25,6 @@ function convertDataToMatrix(data,headers){
   return matrixData;
 }
 
-function createDataPacket(chartData, chartHeaders, chartTitle, chartSubTitle, chartXAxisTitle, chartYAxisTitle){
-  let data = {};
-  data["chartData"] = chartData;
-  data["headers"] = chartHeaders;
-  data["chartTitle"] = chartTitle;
-  data["chartSubTitle"] = chartSubTitle;
-  data["chartXAxisTitle"] = chartXAxisTitle;
-  data["chartYAxisTitle"] = chartYAxisTitle;
-  return data;
-}
-
 function generateChartData(data,headers){
   let matrixData = convertDataToMatrix(data,headers);
   console.log(matrixData);
@@ -43,7 +32,7 @@ function generateChartData(data,headers){
   return chartdata;
 }
 
-function generateOptions(chartTitle,chartSubtitle,chartXAxisTitle,chartYAxisTitle,chartWidth=900,chartHeight=300){
+function generateOptions(chartTitle,chartSubtitle,chartXAxisTitle,chartYAxisTitle){
   let options = {
     title: chartTitle,
     subtitle: chartSubtitle,
@@ -53,8 +42,8 @@ function generateOptions(chartTitle,chartSubtitle,chartXAxisTitle,chartYAxisTitl
     vAxis: {
       title: chartYAxisTitle,
     },
-    width: chartWidth,
-    height: chartHeight,
+    width: 900,
+    height: 600,
     legend: {
       position: 'none'
     },
@@ -62,93 +51,89 @@ function generateOptions(chartTitle,chartSubtitle,chartXAxisTitle,chartYAxisTitl
   return options;
 }
 
-function plotBarChart(data, chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotBarChart(chartData, chartOptions, chartDivID){
   chartOptions["bars"] = 'horizontal';
   var chart = new google.visualization.BarChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
-function plotColumnChart(data, chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotColumnChart(chartData, chartOptions, chartDivID){
   var chart = new google.visualization.ColumnChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
-function plotPieChart(data,chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotPieChart(chartData, chartOptions ,chartDivID){
   chartOptions["pieHole"] = 0.5;
   chartOptions["pieSliceText"] = 'label';
   var chart = new google.visualization.PieChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
-function plotBubbleChart(data,chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotBubbleChart(chartData, chartOptions ,chartDivID){
   var chart = new google.visualization.BubbleChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
-function plotLineChart(data,chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotLineChart(chartData, chartOptions ,chartDivID){
   var chart = new google.visualization.LineChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
-function plotGeoChart(data,chartDivID){
-  let chartData = generateChartData(data["chartData"],data["headers"]);
-  let chartOptions = generateOptions(data["chartTitle"],data["chartSubTitle"],data["chartXAxisTitle"],data["chartYAxisTitle"]);
+function plotGeoChart(chartData, chartOptions ,chartDivID){
   var chart = new google.visualization.GeoChart(document.getElementById(chartDivID));
   chart.draw(chartData, chartOptions);
 }
 
 function plotMostWinsChart(chartData){
-  let data = createDataPacket(chartData,["Winning Team","Number of Match Wins"],"Most Winning IPL Teams","Number of match wins by every IPL team","Number of Match Wins","Winning Team");
-  plotBarChart(data, "mostMatchWins")
+  let data = generateChartData(chartData,["Winning Team","Number of Match Wins"]); 
+    let chartOptions = generateOptions("Most Winning IPL Teams","Number of match wins by every IPL team","Number of Match Wins","Winning Team");
+  plotBarChart(data, chartOptions, "mostMatchWins")
 }
 
 function plotMostTossWinsChart(chartData){
-  let data = createDataPacket(chartData,["Winning Team","Number of Toss Wins"],"Most Toss Winning IPL Teams","Number of toss wins by every IPL team","Number of Toss Wins","Winning Team");
-  plotBarChart(data, "mostTossWins");
+  let data = generateChartData(chartData,["Winning Team","Number of Toss Wins"]); 
+  let chartOptions = generateOptions("Most Toss Winning IPL Teams","Number of toss wins by every IPL team","Number of Toss Wins","Winning Team");
+  plotBarChart(data, chartOptions, "mostTossWins");
 }
 
 function plotMostSeasonWinsChart(chartData){
-  let data = createDataPacket(chartData,["Winning Team","Number of Season Wins"],"Most Season Winning IPL Teams","Number of season wins by every IPL team","Most Season Winning IPL Teams","Number of season wins by every IPL team","Number of Season Wins","Winning Team");
-  plotBarChart(data, "mostSeasonWins");
+  let data = generateChartData(chartData,["Winning Team","Number of Season Wins"]);
+  let chartOptions = generateOptions("Most Season Winning IPL Teams","Number of season wins by every IPL team","Most Season Winning IPL Teams","Number of season wins by every IPL team","Number of Season Wins","Winning Team");
+  plotBarChart(data, chartOptions, "mostSeasonWins");
 }
 
 function plotAverageScoreChart(chartData){
-  let data = createDataPacket(chartData,["Team","Average Score"],"Average Score of IPL Teams","Average Score of every IPL team over all seasons","Team","Average Score");
-  plotPieChart(data, "averageScore");
+  let data = generateChartData(chartData,["Team","Average Score"]);
+  let chartOptions = generateOptions("Average Score of IPL Teams","Average Score of every IPL team over all seasons","Team","Average Score");
+  plotPieChart(data, chartOptions, "averageScore");
 }
 
 function plotFinalScoreBatFirstChart(chartData){
-  let data = createDataPacket(chartData,["Team","Final Match Score", { type: "string", role: "annotation" }],"Final Match Score of IPL Teams - Bat First","Total scored by the team which batted first in the final of the season","Team","Final Match Score");
-  plotColumnChart(data, "finalScoreBatFirst");
+  let data = generateChartData(chartData,["Team","Final Match Score", { type: "string", role: "annotation" }]);
+  let chartOptions = generateOptions("Final Match Score of IPL Teams - Bat First","Total scored by the team which batted first in the final of the season","Team","Final Match Score");
+  plotColumnChart(data, chartOptions, "finalScoreBatFirst");
 }
 
 function plotMatchesStadiumChart(chartData){
-  let data = createDataPacket(chartData,["Stadium","Number of Matches"],"Number of IPL Matches per stadium","A stadium wise distribution of IPL matches","Stadium","Number of Matches");
-  plotColumnChart(data, "mostMatchesStadiumWise");
+  let data = generateChartData(chartData,["Stadium","Number of Matches"]);
+  let chartOptions = generateOptions("Number of IPL Matches per stadium","A stadium wise distribution of IPL matches","Stadium","Number of Matches");
+  plotColumnChart(data, chartOptions, "mostMatchesStadiumWise");
 }
 
 function plotMatchesCityChart(chartData){
-  let data = createDataPacket(chartData,["City","Number of Matches"],"Number of IPL Matches per city","A city wise distribution of IPL matches","City","Number of Matches");
-  plotGeoChart(data, "mostMatchesCityWise");
+  let data = generateChartData(chartData,["City","Number of Matches"]);
+  let chartOptions = generateOptions("Number of IPL Matches per city","A city wise distribution of IPL matches","City","Number of Matches");
+  plotGeoChart(data, chartOptions, "mostMatchesCityWise");
 }
 
 function plotScoreTeams(chartData, scoreType, scoreTypeDiv){
   for (let team in chartData){
-    let data = createDataPacket(chartData[team],["Season",`${scoreType} Score`],`${scoreType} Score of ${team} over all seasons`,`Per Season ${scoreType} total`,"Season",`${scoreType} Score`);
+    let data = generateChartData(chartData[team],["Season",`${scoreType} Score`]);
+    let chartOptions = generateOptions(`${scoreType} Score of ${team} over all seasons`,`Per Season ${scoreType} total`,"Season",`${scoreType} Score`);
     let perTeamDiv = document.createElement("div");
     perTeamDiv.id = `${team}${scoreType}Score`;
     document.getElementById(scoreTypeDiv).appendChild(perTeamDiv);
-    plotLineChart(data, perTeamDiv.id);
+    plotLineChart(data, chartOptions, perTeamDiv.id);
   }
 }
 
