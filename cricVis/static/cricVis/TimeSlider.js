4445653c4b65c5352bd3f10a8f95e4e8fff3ac43
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-23 16:43:27 +0530

    feat: add function to create time slider

--- cricVis/static/cricVis/TimeSlider.js
+++ cricVis/static/cricVis/TimeSlider.js
@@ -5,6 +5,14 @@ class TimeSlider{
     this.sliderIDNumber = sliderIDNumber;
     this.chartData = chartData;
   }
+  createTimeSlider(){
+    const sliderElement = this.createHTMLElement("input", `timeSlider${this.sliderIDNumber}`, "timeSlider");
+    const sliderBubble = this.createHTMLElement("span", `timeSliderValueBubble${this.sliderIDNumber}`, "timeSliderValueBubble");
+    let sliderContainer = this.createHTMLElement("div", `timeSliderContainer${this.sliderIDNumber}`, "timeSliderContainer");
+    sliderContainer.appendChild(sliderBubble);
+    sliderContainer.appendChild(sliderElement);
+    document.getElementById(this.carouselID).appendChild(sliderContainer);
+  }
   createHTMLElement(elementType, elementID, elementClass){
     let element = document.createElement(elementType);
     element.id = elementID;
