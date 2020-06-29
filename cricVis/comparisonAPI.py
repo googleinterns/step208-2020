b2371e71b1ff342385ae391a204cec6df405fb8a
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-29 13:41:37 +0530

    feat: add function to convert column names to heading names

--- cricVis/comparisonAPI.py
+++ cricVis/comparisonAPI.py
@@ -4,4 +4,11 @@ def getComprisonData(tableName, entityID1, entityID2):
 
 def getPlayerData(tableName, playerID):
     playerData = ref.child(tableName).child(playerID).get()
+
+def getHeadingNames(columnName):
+    columnNameList = columnName.split("_")
+    headingName = ""
+    for name in columnNameList:
+        headingName += '%s ' % (name.capitalize())
+    return headingName
     
