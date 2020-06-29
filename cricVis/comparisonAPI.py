e672fbaa9a51ef47cadcf4fa83f63555bb5ab3a0
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-29 13:51:25 +0530

    feat: add function to get chart data for players

--- cricVis/comparisonAPI.py
+++ cricVis/comparisonAPI.py
@@ -10,8 +10,14 @@ def getPlayerCardData(playerData, playerType):
     playerCardData[getHeadingNames("player_name")] = playerData["player_name"]
     playerCardData[getHeadingNames("birth_date")] = playerData["birth_date"]
     playerCardData[getHeadingNames(playerType)] = playerData[playerType]
-    playerCardData["team"] = playerData["team"]
+    playerCardData[getHeadingNames("team")] = playerData["team"]
     return playerCardData
+
+def getChartData(matchTypeData):
+    chartData = {}
+    for data in matchTypeData:
+        chartData[getHeadingNames(data)] = matchTypeData[data]
+    return chartData
 
 def getHeadingNames(columnName):
     columnNameList = columnName.split("_")
