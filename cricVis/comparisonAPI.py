67c87b87e54051a721dd71d391295caf6bbd7209
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-29 13:45:33 +0530

    feat: add function to get card data for players

--- cricVis/comparisonAPI.py
+++ cricVis/comparisonAPI.py
@@ -5,6 +5,14 @@ def getComprisonData(tableName, entityID1, entityID2):
 def getPlayerData(tableName, playerID):
     playerData = ref.child(tableName).child(playerID).get()
 
+def getPlayerCardData(playerData, playerType):
+    playerCardData  = {}
+    playerCardData[getHeadingNames("player_name")] = playerData["player_name"]
+    playerCardData[getHeadingNames("birth_date")] = playerData["birth_date"]
+    playerCardData[getHeadingNames(playerType)] = playerData[playerType]
+    playerCardData["team"] = playerData["team"]
+    return playerCardData
+
 def getHeadingNames(columnName):
     columnNameList = columnName.split("_")
     headingName = ""
