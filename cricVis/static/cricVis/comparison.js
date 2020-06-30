8f266ca95a125a4306e774c92216fbef1b11d973
IshaGupta18 <ishagupta1828@gmail.com>
2020-06-30 11:18:21 +0530

    feat: add function to create autofill

--- cricVis/static/cricVis/comparison.js
+++ cricVis/static/cricVis/comparison.js
@@ -2,3 +2,11 @@ var autofillData;
 function receiveAutofillData(data){
   autofillData = data;
 }
+function createAutofill(sourceData, inputID, type){
+  $(`#${inputID}`).attr('disabled', false);
+  $(`#${inputID}`).attr('placeholder', `Enter ${type} name...`);
+  $(`#${inputID}`).autocomplete({
+    minLength:2,    
+    source: sourceData
+ });
+}
