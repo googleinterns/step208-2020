1e3a5701759391bf8d294b9491cb91910ba8e1dc
IshaGupta18 <ishagupta1828@gmail.com>
2020-07-01 16:12:02 +0530

    feat: add function to create element for one field

--- cricVis/static/cricVis/comparison.js
+++ cricVis/static/cricVis/comparison.js
@@ -28,3 +28,21 @@ function createHTMLElement(type, elementClass=null, elementID=null){
   if (elementClass) element.className = elementClass;
   return element;
 }
+
+function createStatDiv(chartDivID, field, fieldValue1, fieldValue2){
+  const parentDiv = createHTMLElement("div", "list-group-item list-group-item-action flex-column align-items-center");
+  const chartStatsDiv = createHTMLElement("div", "d-flex w-100 justify-content-around");
+  const chartDiv = createHTMLElement("div", "comparisonChartDiv", chartDivID);
+  const stat1 = createHTMLElement("p", "statTag");
+  const stat2 = createHTMLElement("p", "statTag");
+  stat1.innerText = fieldValue1;
+  stat2.innerText = fieldValue2;
+  const chartStatsDivHeading = createHTMLElement("h4", "chartStatsDivHeading");
+  chartStatsDivHeading.innerText = field;
+  chartStatsDiv.appendChild(stat1);
+  chartStatsDiv.appendChild(chartDiv);
+  chartStatsDiv.appendChild(stat2);
+  parentDiv.appendChild(chartStatsDivHeading);
+  parentDiv.appendChild(chartStatsDiv);
+  return parentDiv;
+}
